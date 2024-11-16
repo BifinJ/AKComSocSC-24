57e5ed1027ba51471389b8881a88c42278c26f22
BifinJ <jamesbifin@gmail.com>
2024-11-16 18:44:41 +0530

    content added

--- src/app/components/AboutCard.tsx
+++ src/app/components/AboutCard.tsx
@@ -8,13 +8,13 @@ export default function AboutCard() {
         },
         {
             icon: "https://img.icons8.com/ios-glyphs/30/multiple-messages.png",
-            title: "lorem Ipsum dolor",
-            content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi."
+            title: "About IEEE",
+            content: "IEEE is the world’s largest technical professional organization advancing technology for humanity's benefit. IEEE provides access to industry-leading publications, conferences, and standards to keep members on the cutting edge. With over 400,000 members in 160+ countries, IEEE fosters innovation, collaboration, and technological excellence, empowering professionals and students to turn ideas into impact."
         },
         {
             icon: "https://img.icons8.com/ios-glyphs/30/multiple-messages.png",
-            title: "lorem Ipsum dolor",
-            content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi."
+            title: "About ComSocKC",
+            content: "The IEEE Communications Society (ComSoc) is a leading global community dedicated to advancing communications and networking technology. With a focus on fostering research, innovation, and collaboration, IEEE ComSoc connects engineers, researchers, and industry professionals across various fields of communication. IEEE ComSoc keeps members at the forefront of wireless, networking, IoT, and data science advancements."
         },
     ]
     return (
@@ -24,12 +24,12 @@ export default function AboutCard() {
                 key={index}
                 className={`relative flex flex-col gap-1 items-center p-2 md:p-4 w-full md:w-[520px] h-72 xs:h-64 ${
                 index === 1
-                    ? "bg-[url('/images/about-card-bg.png')] bg-cover bg-center scale-100 md:scale-105 transform transition-transform duration-300 z-30 text-c1"
+                    ? "bg-[url('/images/about-card-bg.png')] bg-cover bg-center scale-100 md:scale-110 transform transition-transform duration-300 z-30 text-c1"
                     : "bg-c2 z-20 text-c5"
                 } ${index === 0 ? "md:pr-8" : ""} ${index === 2 ? "md:pl-8" : ""}`}
             >
                 {index === 1 && (
-                <div className="absolute inset-0 bg-c3 opacity-60"></div>
+                <div className="absolute inset-0 bg-c3 opacity-70"></div>
                 )}
                 {/* Content */}
                 {/* <img
@@ -37,7 +37,7 @@ export default function AboutCard() {
                 alt=""
                 className={` w-6 md:w-8 h-6 md:h-8 relative ${index === 1 ? "text-c1" : "text-cwhite"}`}
                 /> */}
-                <h3 className="text-lg font-semibold relative">
+                <h3 className="text-lg font-bold relative">
                 {card.title}
                 </h3>
                 <p className="text-sm relative text-justify">
--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -7,23 +7,23 @@ export default function Footer() {
         <div className=" w-full md:w-3/5 text-center md:text-left"> 
           <h2 className=" font-futura font-bold text-c5 text-2xl md:text-5xl sm:text-4xl xs:text-3xl">AKComSocSC-24</h2>
           <p className="text-sm mt-2 max-w-l font-open pt-4 text-justify md:text-left">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+            Join us at AKComSocSC'24, where innovation meets collaboration. Explore the latest advancements in communications technology, network with industry experts, and unleash your potential. Shaping the future of communications, together.
           </p>
         </div>
         <div className=" w-full md:w-2/5 flex flex-col items-center justify-center text-center mt-8 md:mt-0">
           <h3 className="text-lg font-semibold mb-1 font-futura">CONTACT US</h3>
           <p className="font-open text-sm md:text-base font-normal mt-1">
-            Phone: <a href="tel:+919876543210" className="hover:underline">+91 9876543210</a>
+            Anjali Vijay: <a href="tel:+917994807141" className="hover:underline">+91 7994807141</a>
           </p>
           <p className="font-open text-sm md:text-base font-normal mt-1">
-            Email: <a href="mailto:sample@ieee.org" className="hover:underline">sample@ieee.org</a>
+            Hanna Elza John: <a href="tel:+918921931121" className="hover:underline">+91 8921931121</a>
           </p>
         </div>
       </div>
 
       <div className="w-full bg-[#0d3673] bg-opacity-10 py-4 text-center text-c4 font-open">
         <p className=" text-xs md:text-sm">2024 ©AKComSocSC&apos;24. All Rights Reserved.</p>
-        <p className=" text-xs md:text-sm">Designed and Developed by AKComSocSC&apos;24 WebTeam</p>
+        <p className=" text-xs md:text-sm">Designed and Developed by AKComSocSC&apos;24 Web Team</p>
       </div>
     </footer>
   );
